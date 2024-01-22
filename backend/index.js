30cc71199c024d9579260e3ae287d5c19df9d7f5
Hasan <thehasandev@gmail.com>
2024-01-22 19:33:32 +0600

    given data frontend to backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+
+app.post('/create', function (req, res) {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+
+app.listen(8000,()=>{
+    console.log("Port is connected");
+})
